6c150be4d79467505eb695c2d42ecee6f35e488d
Pablo Lluch <pablo.lluch@email.com>
2016-03-23 20:55:57 -0300

    Add missing deploy configs

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,7 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
+require 'dotenv'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -24,3 +25,6 @@ module EcheckitApi
     config.active_record.raise_in_transactional_callbacks = true
   end
 end
+
+Dotenv.load
+
