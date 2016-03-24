7495e56bfb336af2f0b07a82af343e0ff58581b6
Pablo Lluch <pablo.lluch@gmail.com>
2016-03-24 20:27:24 -0300

    Fix email

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,23 +29,22 @@ class User < ActiveRecord::Base
     :recoverable, :validatable
 
   validates :email, uniqueness: true, presence: true
-  validates :rut, uniqueness: true, presence: true, on: :update
-  validates :first_name, presence: true, on: :update
-  validates :last_name, presence: true, on: :update
-  validates :phone_number, presence: true, on: :update
+  validates :rut, uniqueness: true, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :phone_number, uniqueness: true, allow_nil: true
 
   def send_reset_password_instructions
     token = set_reset_password_token
     file = Tempfile.new('reset_password')
-    file.write("Subject: 'Solicitud para reestablecer tu contraseña de eCheckit'")
-    file.write("From: From: <s2go@echeckit.cl>")
-    file.write("To: #{full_name}<#{email}>")
+    file.write("Subject: 'Solicitud para reestablecer tu contraseña de eCheckit'\n")
+    file.write("From: Solutions2Go<s2go@echeckit.cl>\n")
+    file.write("To: #{full_name}<#{email}>\n")
     file.write("Hola #{full_name}!\n\n")
     file.write("Has solicitado reestablecer tu contraseña.\n\n")
     file.write("Para realizar la operacion introduce el siguente código en la aplicación: #{token}\n\n\n")
     file.write("Si no has solicitado reestablecer tu contraseña, por favor ignora este email")
     file.close
-
     system "sendmail -t -f #{email} -v -i < #{file.path}"
     file.unlink
   end
