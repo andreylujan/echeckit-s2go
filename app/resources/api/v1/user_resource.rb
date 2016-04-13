7968bf23d72826f95b5d2c88239fd9b8bc439c96
Pablo Lluch <pablo.lluch@email.com>
2016-04-13 17:35:52 -0300

    Allow picture param

--- app/resources/api/v1/user_resource.rb
+++ app/resources/api/v1/user_resource.rb
@@ -1,6 +1,6 @@
 class Api::V1::UserResource < JSONAPI::Resource
   attributes :rut, :first_name, :last_name, :phone_number,
-    :password, :password_confirmation, :email
+    :password, :password_confirmation, :email, :picture
 
   def fetchable_fields
     super - [ :password, :password_confirmation ]
