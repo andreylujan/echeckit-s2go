cedd4b5bcd338b1cbba077cbab2db2b896221ca6
Pablo Lluch <pablo.lluch@email.com>
2016-04-22 11:08:04 -0300

    Picture -> image

--- app/resources/api/v1/user_resource.rb
+++ app/resources/api/v1/user_resource.rb
@@ -1,7 +1,7 @@
 class Api::V1::UserResource < JSONAPI::Resource
   attributes :rut, :first_name, :last_name, :phone_number,
     :password, :password_confirmation, :email, :image, :role_id,
-    :picture
+    :image
 
   def fetchable_fields
     super - [ :password, :password_confirmation ]
