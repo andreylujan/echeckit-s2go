2cf594e1cd5f55119eaa50f71a7af31c7fddcdb5
Pablo Lluch <pablo.lluch@gmail.com>
2016-05-02 14:41:34 -0300

    Pagination config

--- config/initializers/jsonapi.rb
+++ config/initializers/jsonapi.rb
@@ -1,4 +1,8 @@
 JSONAPI.configure do |config|
   # built in key format options are :underscored_key, :camelized_key and :dasherized_key
   config.json_key_format = :underscored_key
+  config.default_paginator = :paged
+
+  config.default_page_size = 10
+  config.maximum_page_size = 30
 end
