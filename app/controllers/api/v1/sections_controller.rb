decfec815ef6bec16980e8739e3994b19d900dad
Pablo Lluch <pablo.lluch@gmail.com>
2016-05-17 12:09:07 -0400

    Order sections by position

--- app/controllers/api/v1/sections_controller.rb
+++ app/controllers/api/v1/sections_controller.rb
@@ -5,7 +5,7 @@ class Api::V1::SectionsController < ApplicationController
   def index  	
   	report_type_id = params.require(:report_type_id)
   	@sections = ReportType.includes(sections: [ :section_type, subsections: [:data_parts]])
-  	.find(report_type_id).sections
+  	.find(report_type_id).sections.order(:position)
   	
   	render json: @sections, include: 'subsections.data_parts'
   end
