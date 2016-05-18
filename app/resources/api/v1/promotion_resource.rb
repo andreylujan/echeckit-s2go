ebe64a1865d7bdcef990f8d91568874fdae5f40c
Pablo Lluch <pablo.lluch@email.com>
2016-05-18 11:49:38 -0400

    Return dealer_ids and zone_ids with promotions

--- app/resources/api/v1/promotion_resource.rb
+++ app/resources/api/v1/promotion_resource.rb
@@ -1,5 +1,5 @@
 class Api::V1::PromotionResource < JSONAPI::Resource
-  attributes :title, :start_date, :end_date, :html
+  attributes :title, :start_date, :end_date, :html, :dealer_ids, :zone_ids
   has_one :checklist, always_include_linkage_data: false
   before_create :set_creator
 
