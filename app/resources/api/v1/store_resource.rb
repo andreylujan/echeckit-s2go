642a9c41be7960a1b49a24d00f5b9c789920d83a
Pablo Lluch <pablo.lluch@email.com>
2016-05-18 18:29:15 -0400

    Store name

--- app/resources/api/v1/store_resource.rb
+++ app/resources/api/v1/store_resource.rb
@@ -5,6 +5,13 @@ class Api::V1::StoreResource < JSONAPI::Resource
   has_one :zone
   has_one :dealer
   
+  def name
+  	if @model.zone.nil? or @model.dealer.nil?
+  		return @model.name
+  	end
+  	@model.name + " - " + @model.dealer.name + " - " + @model.zone.name
+  end
+
   def fetchable_fields
     super
   end
