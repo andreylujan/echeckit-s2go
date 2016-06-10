a93dbf57bcb266a97c9491ded3161a63627568c5
Pablo Lluch <pablo.lluch@email.com>
2016-06-10 16:23:00 -0400

    Send message sent_at

--- app/resources/api/v1/message_resource.rb
+++ app/resources/api/v1/message_resource.rb
@@ -2,10 +2,14 @@ class Api::V1::MessageResource < BaseResource
 
 
 	attributes :read, :read_at, :title, :html, :resource_id,
-		:message_action
+		:sent_at, :message_action, 
 
 	def message_action
 		@model.message_action.as_json.except("created_at", "updated_at")
+	end
+
+	def sent_at
+		@model.broadcast.created_at
 	end
 
 	def self.records(options = {})
