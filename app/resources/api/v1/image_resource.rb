16594809182469d99bed05101f4732da126a23dd
Pablo Lluch <pablo.lluch@email.com>
2016-06-14 11:30:05 -0400

    Add url attribute

--- app/resources/api/v1/image_resource.rb
+++ app/resources/api/v1/image_resource.rb
@@ -1,7 +1,11 @@
 # -*- encoding : utf-8 -*-
 class Api::V1::ImageResource < JSONAPI::Resource
   
-  
+  attributes :url
+
+	def url
+		@model.image.url
+	end  
   
   def fetchable_fields
     super
