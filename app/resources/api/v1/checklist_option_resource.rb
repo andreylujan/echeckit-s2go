154d4ea5e7e51b818307bf5accf8d71b8851caad
Pablo Lluch <pablo.lluch@email.com>
2016-06-17 16:28:32 -0400

    is_binary

--- app/resources/api/v1/checklist_option_resource.rb
+++ app/resources/api/v1/checklist_option_resource.rb
@@ -1,5 +1,13 @@
 class Api::V1::ChecklistOptionResource < JSONAPI::Resource
-	attributes :name, :icon
+	attributes :name, :icon, :is_binary
 
 	has_one :detail
+
+	def is_binary
+		if @model.data["is_binary"]
+			@model.data["is_binary"]
+		else
+			false
+		end
+	end
 end
