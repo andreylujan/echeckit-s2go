a50829a238813d3517eaa650ad02c350c587e366
Pablo Lluch <pablo.lluch@gmail.com>
2016-07-01 12:22:06 -0400

    Validations

--- app/controllers/api/v1/sale_goals_controller.rb
+++ app/controllers/api/v1/sale_goals_controller.rb
@@ -9,6 +9,7 @@ class Api::V1::SaleGoalsController < ApplicationController
 		year = params.require(:year)
 		csv_data.shift
 		errors = []
+		date = DateTime.new(year.to_i, month.to_i)
 		csv_data.each_with_index do |goal_row, index|
 
 			store_code = goal_row[0]
@@ -33,7 +34,17 @@ class Api::V1::SaleGoalsController < ApplicationController
 					store_code: [ "Tienda con código indicado no existe" ]
 				}
 			else
-				data[:meta][:success] = true
+				goal = SaleGoal.find_or_initialize_by(store: store, 
+					goal_date: date)
+				goal.monthly_goal = monthly_goal
+				goal.save
+				if goal.errors.any?
+					data[:meta][:success] = false
+					data[:meta][:errors] = goal.errors.as_json
+				else
+					data[:meta][:success] = true
+				end
+				
 			end
 			errors << data
 		end
