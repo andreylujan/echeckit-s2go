dfe9675becff6df5f6e0b0cec05f3e487e942355
Pablo Lluch <pablo.lluch@email.com>
2016-08-03 12:13:15 -0400

    Include record and page count

--- config/initializers/jsonapi.rb
+++ config/initializers/jsonapi.rb
@@ -10,6 +10,13 @@ JSONAPI.configure do |config|
 
   config.top_level_links_include_pagination = false
   config.always_include_to_one_linkage_data = false
+
+  config.top_level_meta_include_record_count = true
+
+  config.top_level_meta_include_page_count = true
+  # config.top_level_meta_page_count_key = :page_count
+
+  # config.top_level_meta_record_count_key = :record_count
 end
 
 Rails.configuration.to_prepare do
