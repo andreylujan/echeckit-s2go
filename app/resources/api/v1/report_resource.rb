8e275de0fe3d47a83b2a8c0ec33161a9a6dc82ed
Pablo Lluch <pablo.lluch@gmail.com>
2016-08-26 20:31:15 -0300

    Task list

--- app/resources/api/v1/report_resource.rb
+++ app/resources/api/v1/report_resource.rb
@@ -2,8 +2,10 @@
 class Api::V1::ReportResource < JSONAPI::Resource
   attributes :dynamic_attributes, :creator_id, :created_at, :limit_date,
   :finished, :assigned_user_id, :pdf, :pdf_uploaded,
-  :task_start, :task_end, :title, :description
+  :task_start, :title, :description
 
+  has_one :assigned_user
+  
   def custom_links(options)
     {self: nil}
   end
@@ -22,10 +24,35 @@ class Api::V1::ReportResource < JSONAPI::Resource
     end
   end
 
-  filters :assigned_user_id
+  filters :assigned_user_id, :finished, :dealer_ids, :zone_ids, :store_ids
   
   filter :creator_id, apply: ->(records, value, _options) {
     records.where('assigned_user_id is NULL')
+  }
+
+  filter :zone_ids, apply: ->(records, value, _options) {
+    if value.is_a? Array and value.length > 0
+      records.joins(:store)
+      .where(stores: { zone_id: value })
+    else
+      records
+    end
+  }
+  filter :dealer_ids, apply: ->(records, value, _options) {
+    if value.is_a? Array and value.length > 0
+      records.joins(:store)
+      .where(stores: { dealer_id: value })
+    else
+      records
+    end
+  }
+  filter :store_ids, apply: ->(records, value, _options) {
+    if value.is_a? Array and value.length > 0
+      records.joins(:store)
+      .where(stores: { id: value })
+    else
+      records
+    end
   }
 
   def fetchable_fields
