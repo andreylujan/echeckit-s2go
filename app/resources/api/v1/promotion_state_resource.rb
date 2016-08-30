aeacd3c2788e886b31273ff84666b4bbf324f2ae
Pablo Lluch <pablo.lluch@email.com>
2016-08-29 23:30:17 -0300

    Promotion state title

--- app/resources/api/v1/promotion_state_resource.rb
+++ app/resources/api/v1/promotion_state_resource.rb
@@ -2,7 +2,7 @@ class Api::V1::PromotionStateResource < BaseResource
   attributes :activated_at, :store_name, :dealer_name, :zone_name,
     :pdf, :pdf_uploaded, :activated, :activated_at,
     :activator_name, :creator_name, :promotion_id,
-    :start_date, :end_date
+    :start_date, :end_date, :title
 
   filter :activated, apply: ->(records, value, _options) {
     if value.is_a? Array and value.length > 0
