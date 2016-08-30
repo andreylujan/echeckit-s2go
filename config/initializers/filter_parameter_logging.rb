a48a16a8d24fe599ba2991ce066d2d36006f32f1
Pablo Lluch <pablo.lluch@gmail.com>
2016-08-30 18:42:25 -0300

    Don't filter

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -2,4 +2,4 @@
 # Be sure to restart your server when you modify this file.
 
 # Configure sensitive parameters which will be filtered from the log file.
-Rails.application.config.filter_parameters += [:password, :dynamic_attributes, :image]
+Rails.application.config.filter_parameters += [:password, :image]
