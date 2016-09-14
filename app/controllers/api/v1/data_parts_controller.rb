b6b871553c1aa499403d0e901bc8055dc0501557
Pablo Lluch <pablo.lluch@gmail.com>
2016-09-14 00:11:25 -0300

    Checklist index fix

--- app/controllers/api/v1/data_parts_controller.rb
+++ app/controllers/api/v1/data_parts_controller.rb
@@ -11,6 +11,13 @@ class Api::V1::DataPartsController < Api::V1::JsonApiController
 
 			each_serializer = Object.const_get("#{params[:type]}Serializer")
 			render json: parts, each_serializer: each_serializer
+		elsif params[:filter].present? and params[:filter][:type]
+			type = params[:filter][:type]
+			parts = 
+			current_user.organization.data_parts.where(type: type)
+
+			each_serializer = Object.const_get("#{type}Serializer")
+			render json: parts, each_serializer: each_serializer
 		else
 			super
 		end  	
