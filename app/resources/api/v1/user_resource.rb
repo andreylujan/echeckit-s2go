74a6ad0f8a89ce0825682ee76d23952494e5edf8
Pablo Lluch <pablo.lluch@gmail.com>
2016-10-11 18:16:02 -0300

    Promoters list fix

--- app/resources/api/v1/user_resource.rb
+++ app/resources/api/v1/user_resource.rb
@@ -25,8 +25,15 @@ class Api::V1::UserResource < BaseResource
   }
   filter :store_ids, apply: ->(records, value, _options) {
     if value.is_a? Array and value.length > 0
-      records.joins(:promoted_stores)
-      .where(stores: { id: value })
+      promoters = records.joins(:promoted_stores)
+      .where(stores: { id: value }).uniq
+      supervisors = records.joins(:supervised_stores)
+      .where(stores: { id: value }).uniq
+      instructors = records.joins(:instructed_stores)
+      .where(stores: { id: value }).uniq
+      user_ids = promoters.map { |p| p.id } + supervisors.map { |p| p.id } + instructors.map { |p| p.id }
+      user_ids.uniq!
+      User.where(id: user_ids)
     else
       records
     end
@@ -38,7 +45,7 @@ class Api::V1::UserResource < BaseResource
       # user_ids = User.joins(:promoted_stores).map { |u| u.id } + User.where(role_id: 2).map { |u| u.id }
       # user_ids.uniq!
       # users = User.where(id: user_ids)
-      users = User.joins(:promoted_stores).uniq
+      users = User.all
       if not options[:sort_criteria].present?
         users = users.order('first_name, last_name')
       end
