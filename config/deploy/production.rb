68ab596b29c298b10a071537816a53abeb5d6757
Pablo Lluch <pablo.lluch@gmail.com>
2017-04-10 13:56:36 -0300

    Deploy env variables

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -13,7 +13,7 @@ server '50.16.161.152', port: 60, user: 'ubuntu', roles: [:web, :app, :db]
 set :branch, 'production'
 
 # Default deploy_to directory is /var/www/my_app
-set :deploy_to, '/srv/http/www/echeckit/eretail/api'
+set :deploy_to, "#{ENV['DEPLOY_PATH']}"
 
 set :ssh_options, {
  keys: [ "#{ENV['HOME']}/.ssh/echeckit_production.pem"  ],
