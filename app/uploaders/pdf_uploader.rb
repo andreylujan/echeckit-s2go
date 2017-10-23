f6b3c88a9e725a374e3193714a8b890c7407f122
Pablo Lluch <pablo.lluch@gmail.com>
2017-10-23 02:01:26 -0300

    Last change

--- app/uploaders/pdf_uploader.rb
+++ app/uploaders/pdf_uploader.rb
@@ -55,7 +55,7 @@ class PdfUploader < CarrierWave::Uploader::Base
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
   def filename
-    "#{model.uuid}.pdf"
+    "#{md5}.pdf"
   end
 
 end
