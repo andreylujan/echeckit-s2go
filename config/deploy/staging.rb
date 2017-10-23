89e67aa75e01f96ce012ac5a2e81665548db1194
Pablo Lluch <pablo.lluch@gmail.com>
2017-10-23 00:40:33 -0300

    Staging config

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -8,12 +8,15 @@
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
 
+require 'dotenv'
+Dotenv.load
+
 server '50.16.161.152', port: 60, user: 'ubuntu', roles: [:web, :app, :db]
 
 set :branch, 'staging'
 
 # Default deploy_to directory is /var/www/my_app
-set :deploy_to, '/srv/http/www/echeckit/eretail-staging/api'
+set :deploy_to, "#{ENV['DEPLOY_PATH']}"
 
 set :ssh_options, {
  keys: [ "#{ENV['HOME']}/.ssh/echeckit_production.pem"  ],
